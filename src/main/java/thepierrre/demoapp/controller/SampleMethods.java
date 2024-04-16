30d70f80cac4ad8c50941c4abd205eae1ec3a6a1
thepierrre <piotr.owczarczyk98@gmail.com>
2024-04-16 12:12:47 +0200

    first commit

---
+++ src/main/java/thepierrre/demoapp/controller/SampleMethods.java
@@ -0,0 +1,24 @@
+package thepierrre.demoapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleMethods {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the demo Spring Boot application!";
+    }
+
+    @GetMapping("/add")
+    public int add(@RequestParam int a, @RequestParam int b) {
+        return a + b;
+    }
+
+    @GetMapping("/subtract")
+    public int subtract(@RequestParam int a, @RequestParam int b) {
+        return a - b;
+    }
+}
